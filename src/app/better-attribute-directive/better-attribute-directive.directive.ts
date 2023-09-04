16fae83d962f237e1ca66fc5a18ac6131200e0d6
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-09-04 20:25:01 +0530

    Using HostListener to Listen to Host Events

--- src/app/better-attribute-directive/better-attribute-directive.directive.ts
+++ src/app/better-attribute-directive/better-attribute-directive.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterAttributeDirective]',
@@ -14,5 +14,22 @@ export class BetterAttributeDirectiveDirective {
       'black'
     );
     this.renderer.setStyle(this.UIelement.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseenter') onMouseEnter(eventData: Event) {
+    this.renderer.setStyle(
+      this.UIelement.nativeElement,
+      'background-color',
+      'red'
+    );
+  }
+
+  @HostListener('mouseleave') onMouseLeave(eventData: Event) {
+    this.renderer.setStyle(
+      this.UIelement.nativeElement,
+      'background-color',
+      'transparent'
+    );
+    this.renderer.setStyle(this.UIelement.nativeElement, 'color', 'black');
   }
 }
